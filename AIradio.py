5499ad43bffcace3771fb291400a0dac45e31f9f
山口寛永 <yamaguchikanei@yamaguchikaneinoMacBook-Air.local>
2024-11-18 23:51:40 +0900

    add comments

--- AIradio.py
+++ AIradio.py
@@ -9,6 +9,10 @@ from pydub import AudioSegment
 from pydub.playback import play
 
 def get_sheet():
+    """ 
+    スプレッドシートを取得する関数 
+    
+    """
     # 認証してGoogleスプレッドシートにアクセス
     gc = gspread.service_account(filename="./.config/airadio.json")
     # スプレッドシートを開く (スプレッドシートのURLの最後にあるIDを使用)
@@ -18,6 +22,10 @@ def get_sheet():
     return ws
 
 def make_ngwords_list():
+    """ 
+    NGリストを作成 
+    
+    """
     ng_words = ['事故','死亡','骨折','重傷','殺害','傷害','暴力','被害者','放送事故',\
             'ポルノ','アダルト','セックス','バイブレーター','マスターベーション','オナニー','スケベ','羞恥','セクロス',\
             'エッチ','SEX','風俗','童貞','ペニス','巨乳','ロリ','触手','羞恥','ノーブラ',\
@@ -34,6 +42,10 @@ def make_ngwords_list():
     return ng_words
 
 def check_ngwords(words_list, ng_words):
+    """
+    リストからNGリストに載ってる単語を取り除く。
+
+    """
     filtered_array = [
     row for row in words_list if not any(
         any(ng_word in item for item in row) for ng_word in ng_words
@@ -42,6 +54,11 @@ def check_ngwords(words_list, ng_words):
     return filtered_array
 
 def pick_up_mail(ws,pick_up_num,words_list):
+    """ 
+    リストからランダムにpick_up_um個要素を抽出し、
+    何を抽出したかスプレッドシートに記録する。
+    
+    """
     not_used_list=[x for x in words_list if str(x[4])=="0"]
 
     if len(not_used_list) >= pick_up_num:
@@ -57,6 +74,10 @@ def pick_up_mail(ws,pick_up_num,words_list):
     return choices
 
 def get_reply(client,content):
+    """
+    contentの中身をChatGPTに質問する。
+
+    """
     res= client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[{
@@ -68,6 +89,10 @@ def get_reply(client,content):
     return reply
 
 def input_to_male(content):
+    """
+    田中に投げかける質問文を生成する。
+
+    """
     content = str(content)
     text = "あなたは田中という男の芸人だ。\
         友達の中田と、簡単な会話をしている。\
@@ -75,6 +100,10 @@ def input_to_male(content):
     return text + content
 
 def input_to_female(content):
+    """
+    中田に投げかける質問文を生成する。
+    
+    """
     content = str(content)
     text = "あなたは中村という、常に冷静な女性だ。\
         友達の田村と、簡単な会話をしています。\
@@ -82,17 +111,29 @@ def input_to_female(content):
     return text + content
 
 def man_talks(client,content):
+    """
+    田中に質問文を投げかけて、回答を生成する。
+    
+    """
     txt = input_to_male(content)
     reply = get_reply(client,txt)
     return reply
 
 def female_talks(client,content):
+    """
+    中田に質問文を投げかけて、回答を生成する。
+    
+    """
     txt = input_to_female(content)
     reply = get_reply(client,txt)
     return reply
 
 
 def talk_with_each_other(client,first_content,repeat_time=2):
+    """
+    田中と中田の会話のやりとりをrepeat_time分、生成する。
+    
+    """
     conv_list = []
     content = first_content
     i = 0
@@ -105,6 +146,9 @@ def talk_with_each_other(client,first_content,repeat_time=2):
     return conv_list
 
 def is_charactor_num(num):
+    """
+    voicevoxに渡すキャラクター番号を、入力に応じて返す。
+    """
     if num%2: #もし2で割り切れるなら、女性の声を当てる。
         return 9
     else: #もし2で割り切れないなら、男性の声を当てる。
@@ -114,6 +158,10 @@ def is_charactor_num(num):
 
     
 def get_audio_filepath(reply, charactor_num):
+    """
+    textをvoicevoxに読み上げてもらう。音声ファイルをwavで保存。
+    
+    """
     params = {"text":reply, "speaker":charactor_num}
     res = requests.post(
         f'http://localhost:50021/audio_query',
@@ -136,22 +184,38 @@ def get_audio_filepath(reply, charactor_num):
     #sd.wait()
 
 def get_audio_file(file_path):
+    """
+    wavファイルを呼び出して、AudioSegmentに変換。
+    
+    """
     #音声ファイルをAudioSegmentで出力
-    audio_file = AudioSegment.from_wav(file_path)
+    audio_file = AudioSegment.from_file(file_path)
     return audio_file
 
 def get_audio_reply(reply, charactor_num):
+    """
+    voicevoxに読み上げてもらう音声をAudioSegmentに変換。
+    
+    """
     filepath = get_audio_filepath(reply, charactor_num)
     audio_reply = get_audio_file(filepath)
     return audio_reply
 
 def make_them_speak(client,conv_list):
+    """
+    二人の会話のやりとりを交互に読み上げてもらう。
+    
+    """
     combined_audio_file = AudioSegment.empty()
     for i, conv in enumerate(conv_list):
         combined_audio_file += get_audio_reply(conv,is_charactor_num(i))
     return combined_audio_file
 
 def get_mail_content(content_list):   
+    """
+    メールの内容を取得する。
+    
+    """
     radio_names = list(map(lambda x:x[5], content_list))
     for i in range(len(radio_names)):
         if radio_names[i].strip(" ") == "":
@@ -162,6 +226,10 @@ def get_mail_content(content_list):
     return radio_names,odais
     
 def oogiri(client,ws,oogiri_list):
+    """
+    大喜利のコーナーの文章を生成する。
+    
+    """
     pick_up_list = pick_up_mail(ws,2,oogiri_list)
 
     if len(pick_up_list) < 2:
@@ -187,6 +255,10 @@ def oogiri(client,ws,oogiri_list):
 
 
 def iwasetaikoto(client,ws,iwasetaikoto_list):
+    """
+    AIに言わせたいことのコーナーの文章を生成する。
+    
+    """
     pick_up_list = pick_up_mail(ws,2,iwasetaikoto_list)
 
     if len(pick_up_list) < 2:
@@ -208,6 +280,10 @@ def iwasetaikoto(client,ws,iwasetaikoto_list):
 
 
 def hutsuota(ws,hutsuota_list):
+    """
+    ふつおたのコーナーの文章を生成する。
+    
+    """
     pick_up_list = pick_up_mail(ws,1,hutsuota_list)
     if len(pick_up_list) == 0:
         return ""
@@ -215,11 +291,11 @@ def hutsuota(ws,hutsuota_list):
     content = f'ふつおたが届いています。ラジオネーム、{radio_name[0]}からいただきました。\n{odai[0]}'
     return content
 
-def get_audio_with_fadeout(file_path):
-    audio_file = AudioSegment.from_file(file_path)
-    return audio_file.fade_out(1000)
-
 def get_title(client,content):
+    """
+    タイトルと要約を取得。
+    
+    """
     text = f'次の文章を要約して、人を惹きつけるタイトルをつけてください。\n{content}'
     return get_reply(client,text)
 
@@ -230,7 +306,7 @@ def main():
     mail_data = ws.get_all_values()
     ng_words = make_ngwords_list()
 
-    #ふつおたがあれば最初に読む。
+    #ふつおたがあれば最初に読む。最初のトークの内容を生成。
     hutsuota_list=[x for x in mail_data if "ふつおた" in x[2]]
     hutsuota_list = check_ngwords(hutsuota_list, ng_words)
     first_content = ""
@@ -246,34 +322,40 @@ def main():
         
     print("ふつおたの処理完了")
 
+    #大喜利コーナー
     oogiri_list=[x for x in mail_data if "大喜利" in x[2]]
     oogiri_list = check_ngwords(oogiri_list,ng_words)
     oogiri_conv = []
-    if len(oogiri_list) >= 2:
+    if len(oogiri_list) >= 2: #大喜利のメールが二件以上あれば大喜利のコーナー実施。
         oogiri_conv = oogiri(client,ws,oogiri_list)
     print("大喜利の処理完了")
 
+    #AIに言わせたいことのコーナー
     iwasetaikoto_list=[x for x in mail_data if ("言わせたい" in x[2])or("いわせたい" in x[2])]
     iwasetaikoto_list = check_ngwords(iwasetaikoto_list, ng_words)
     iwasetaikoto_conv = []
-    if len(iwasetaikoto_list) >= 2:
+    if len(iwasetaikoto_list) >= 2: #AIに言わせたいことのメールが二件以上あれば大喜利のコーナー実施。
         iwasetaikoto_conv = iwasetaikoto(client,ws,iwasetaikoto_list)
     print("言わせたいことの処理完了")
 
+    #オープニングのコーナーを生成。
     opening = ["こんにちは！AIの田中です。","こんにちはAIの中田です。",\
                 "この番組はAIパーソナリティの田中と中田でお送りいたします。",\
                 "内容はすべてリスナーの皆様とAIによって作られています。",\
                 "よろしくお願いします。オープニング行きます、AIラジオ"]
     
+    #エンディングのコーナーを生成。
     ending  =  ["AIラジオ、そろそろお別れのお時間です。","大喜利のお題やAIに言わせたいこと、ふつおたのメールお待ちしております。",\
                "件名にコーナー名を書いて送ってください。","宛先はTANAKATA、ドット、AIRADIO、アット、GMAIL、ドット、COM",
                "たなかた、ドット、AIラジオアットジーメールコムとお覚えください。","感想は、ハッシュタグ田中と中田のAIラジオをつけてポストしてください。"\
                "お願いしますー。","以上、ここまでのお相手は中田と","田中でした！今週も聞いてくれてありがとう！","来週もお楽しみに！","さよならー！"]
     
+    #フリートーク（ふつおた）のコーナーを生成。
     conv_list = talk_with_each_other(client,first_content,repeat_time=6) 
     #conv_list = ['「うん、ウサギの耳を持つスパゲッティマシーンが欲しいんだ！」', '「それはユニークなアイデアね。どんな用途を考えているの？」', '「うん、カメラの代わりにスイカを使って、スイカの中から風景を撮ろうと思ってるんだ！」', '「面白いアイデアね、その発想でどんな風景が撮れるか楽しみ。」']
     print("フリートークの内容生成完了")
 
+    #要約文・タイトルを生成。
     subtitle = opening + [first_content] + conv_list + oogiri_conv + iwasetaikoto_conv + ending 
     title = get_title(client,subtitle)
     text = title + "\n-----以下本文-----\n" + str(subtitle)
@@ -282,38 +364,38 @@ def main():
     print("----------------------")
     print("全テキストの生成完了")
 
-    #BGM
-    audio1 = get_audio_with_fadeout("airadio-bgm.mp3")
-    audio2 = get_audio_with_fadeout("airadio-corner-bgm.mp3") - 10
+    #BGMの収録
+    audio1 = get_audio_file("airadio-bgm.mp3").fade_out(1000)
+    audio2 = get_audio_file("airadio-corner-bgm.mp3").fade_out(1000) - 10
     print("BGMの処理完了")
 
 
-    #オープニング
+    #オープニングの収録
     opening_audio = make_them_speak(client,opening) + audio1
     print("オープニングの収録終了")
 
-    #フリートーク
+    #フリートークの収録
     free_talk_audio1 = get_audio_reply(first_content, 9)
     free_talk_audio2 = make_them_speak(client,conv_list)
     free_talk_audio = free_talk_audio1 + free_talk_audio2
     play(free_talk_audio)
     print("フリートークの収録終了")
 
-    #大喜利
+    #大喜利の収録
     if len(oogiri_conv) >= 2:
         oogiri_audio = audio2 + make_them_speak(client,oogiri_conv).fade_out(1000)
     else:
         oogiri_audio = AudioSegment.empty()
     print("大喜利の収録終了")
 
-    #我々に言わせたいこと
+    #我々に言わせたいことの収録
     if len(iwasetaikoto_conv) >= 2:
         iwasetaikoto_audio = audio2 + make_them_speak(client,iwasetaikoto_conv).fade_out(1000)
     else:
         iwasetaikoto_audio = AudioSegment.empty()
     print("言わせたいことの収録終了")
 
-    #エンディング
+    #エンディングの収録
     ending_audio = audio1 + make_them_speak(client,ending)
     print("エンディングの収録終了")
 
